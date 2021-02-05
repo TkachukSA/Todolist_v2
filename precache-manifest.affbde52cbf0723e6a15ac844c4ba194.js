b088595a6b5d96c941b81e0d12ece5ccb8e014ee
asa121asa <crafta.net@gmail.com>
2021-02-05 05:04:43 +0300

    Updates

---
+++ precache-manifest.affbde52cbf0723e6a15ac844c4ba194.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8470c4b511badf9f6f997f1d3bc92d17",
+    "url": "/Todolist_v2/index.html"
+  },
+  {
+    "revision": "a666a2c7e14e3f55e651",
+    "url": "/Todolist_v2/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "83f18c8f6212f45eac17",
+    "url": "/Todolist_v2/static/js/2.eb6b61e3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Todolist_v2/static/js/2.eb6b61e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a666a2c7e14e3f55e651",
+    "url": "/Todolist_v2/static/js/main.8358cbf6.chunk.js"
+  },
+  {
+    "revision": "9554aa622b8c12ac86d7",
+    "url": "/Todolist_v2/static/js/runtime-main.25ec6327.js"
+  }
+]);
